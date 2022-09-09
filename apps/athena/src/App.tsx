3cad1ca1063aa7526a709c3168f378133c7de073
Spark <79936503+spa5k@users.noreply.github.com>
2022-09-09 16:12:36 +0530

    created pokemon models

--- apps/athena/src/App.tsx
+++ apps/athena/src/App.tsx
@@ -1,11 +1,16 @@
+import type { Pokemon } from '@core/models'
 import { Button } from '@core/ui'
 import './App.css'
 
 function App(): JSX.Element {
+	const x: Pokemon = {
+		abilities: [{ ability: { name: 'nice' } }],
+	}
 	return (
 		<div className='App'>
 			<h2>Main Athena app</h2>
 			<Button />
+
 			<p>nice</p>
 		</div>
 	)
---
+++ models/index.ts
@@ -0,0 +1,3 @@
+export * from './Ability'
+export * from './pokemon'
+export * from './Sprites'
