7f3529f243f58105990481ef9f33d62568682956
Spark <79936503+spa5k@users.noreply.github.com>
2022-09-11 00:11:20 +0530

    deploying hera again

--- apps/hera/pages/_app.tsx
+++ apps/hera/pages/_app.tsx
@@ -3,7 +3,8 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+// @ts-ignore
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 	return <Component {...pageProps} />
 }
 
